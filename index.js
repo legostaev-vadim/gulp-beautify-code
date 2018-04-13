f01fb9598b1ce4311007cf4a8e44e3ea9a428779
Vadim <legostaev.vadim@mail.ru>
2018-04-13 16:19:57 +0300

    edit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const beautify = require('js-beautify'),
     through2 = require('through2').obj;
 
 
-module.exports = function(type = 'js', options = {}) {
+module.exports = function(options = {}) {
 
     options.indent_size = options.indent_size || 4;
     options.indent_char = options.indent_char || ' ';
@@ -13,11 +13,11 @@ module.exports = function(type = 'js', options = {}) {
         if (file.isNull()) return callback(null, file);
         if (file.isStream()) return callback(new Error('gulp-beautify-code: Streaming not supported'));
 
-        if (type === 'js') {
+        if (file.extname === '.js') {
             file.contents = Buffer.from(beautify(file.contents.toString(), options));
-        } else if (type === 'css') {
+        } else if (file.extname === '.css') {
             file.contents = Buffer.from(beautify.css(file.contents.toString(), options));
-        } else if (type === 'html') {
+        } else if (file.extname === '.html') {
             file.contents = Buffer.from(beautify.html(file.contents.toString(), options));
         }
 
